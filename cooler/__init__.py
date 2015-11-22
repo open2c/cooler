2ca1ea3b499d9643c2308305b772aa6e05e9b11e
Nezar <nabdennur@gmail.com>
2015-11-22 16:20:23 -0500

    update gitignore

--- cooler/__init__.py
+++ cooler/__init__.py
@@ -4,9 +4,7 @@ from .format import (from_dense, get_slice, get_matrix,
                      get_scaffolds, get_bins, get_metadata)
 from .util import open_hdf5
 from . import genome
-from . import legacy
 
 __version__ = '0.1dev'
 __all__ = ['from_dense', 'get_slice', 'get_matrix', 'get_scaffolds', 
-           'get_bins', 'get_metadata', 'legacy', 'open_hdf5',
-           'genome', 'legacy']
+           'get_bins', 'get_metadata', 'legacy', 'open_hdf5', 'genome']
