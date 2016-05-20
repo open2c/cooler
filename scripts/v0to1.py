189a328d1f9dfbc7c0923d2f844b5508f7a85cac
Nezar <nabdennur@gmail.com>
2016-05-20 07:02:40 -0400

    Update v0to1 script

--- scripts/v0to1.py
+++ scripts/v0to1.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 import sys
 import h5py
 
@@ -7,14 +7,11 @@ infiles = sys.argv[1:]
 for infile in infiles:
     with h5py.File(infile, 'a') as h5:
         print(infile)
-        if 'matrix' in h5:
-            print('renaming matrix')
+        if 'matrix' in h5 and not 'pixels' in h5:
+            print('renaming matrix --> pixels')
             h5['pixels'] = h5['matrix']
-            #del h5['matrix']
 
-        if 'scaffolds' in h5:
-            print('renaming scaffolds')
+        if 'scaffolds' in h5 and not 'chroms' in h5:
+            print('renaming scaffolds --> chroms')
             h5['chroms'] = h5['scaffolds']
-            #del h5['scaffolds']
-
 
