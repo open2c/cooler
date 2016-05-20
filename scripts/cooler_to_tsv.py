7852b2d12023bea7dbef027e3da5cfbc61a39135
Nezar <nabdennur@gmail.com>
2016-05-20 07:14:10 -0400

    Py2/3 compat for cooler_to_tsv.py and support gzip output

--- scripts/cooler_to_tsv.py
+++ scripts/cooler_to_tsv.py
@@ -1,51 +1,56 @@
-#!/usr/bin/python
-
-import sys
+#!/usr/bin/env python
+from __future__ import division, print_function
 import argparse
+import gzip
+import sys
+
 
 def main():
     parser = argparse.ArgumentParser(description="""
     
     python cooler_to_tsv.py cooler_file output_file
-
+​
     Convert a cooler file to a tsv file with chromosome positions and bin counts like this:
-
+​
     chr1     5000    10000    chr1    5000    10000    1.0
+
 """)
 
     parser.add_argument('cooler_file', nargs=1)
     parser.add_argument('output_file', nargs=1)
     #parser.add_argument('-o', '--options', default='yo',
-    #					 help="Some option", type='str')
+    #                    help="Some option", type='str')
     #parser.add_argument('-u', '--useless', action='store_true', 
-    #					 help='Another useless option')
+    #                    help='Another useless option')
 
     args = parser.parse_args()
-
 
     import cooler
     import numpy as np
 
     cooler_file = args.cooler_file[0]
-    print >>sys.stderr, "Loading cooler file... " + cooler_file
-    c = cooler.Cooler(args.cooler_file[0])
-    print >>sys.stderr, "done"
+    print("Loading cooler file... " + cooler_file, file=sys.stderr)
 
-    chunksize = 1000000
+    c = cooler.Cooler(args.cooler_file[0])
+    print("done", file=sys.stderr)
+
+    chunksize = 10000000
     spans = np.arange(0, c.info['nnz']+chunksize, chunksize)
     table = c.pixeltable(join=True)
 
     if args.output_file == '-':
         f = sys.stdout
+    elif args.output_file[0].endswith('.gz'):
+        f = gzip.open(args.output_file[0], 'wt')
     else:
         f = open(args.output_file[0], 'w')
 
     for lo, hi in zip(spans[:-1], spans[1:]):
-        print(lo, hi)
+        print('loading chunk {}..{}'.format(lo, hi), file=sys.stderr)
         pix = table[lo:hi]
+        print('writing', file=sys.stderr)
         pix.to_csv(f, sep='\t', index=False, header=False)    
+
 
 if __name__ == '__main__':
     main()
-
-
