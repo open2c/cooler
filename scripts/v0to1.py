e9121dc5c5e641ac7a0524eb3f198dc7b2ca4efe
Nezar <nabdennur@gmail.com>
2016-08-04 17:58:34 -0400

    Change format version metadata in v0to1

--- scripts/v0to1.py
+++ scripts/v0to1.py
@@ -7,11 +7,15 @@ infiles = sys.argv[1:]
 for infile in infiles:
     with h5py.File(infile, 'a') as h5:
         print(infile)
-        if 'matrix' in h5 and not 'pixels' in h5:
-            print('renaming matrix --> pixels')
-            h5['pixels'] = h5['matrix']
+        if 'format-version' in h5.attrs and h5.attrs['format-version'] < 1:
 
-        if 'scaffolds' in h5 and not 'chroms' in h5:
-            print('renaming scaffolds --> chroms')
-            h5['chroms'] = h5['scaffolds']
+            if 'matrix' in h5 and not 'pixels' in h5:
+                print('renaming matrix --> pixels')
+                h5['pixels'] = h5['matrix']
+
+            if 'scaffolds' in h5 and not 'chroms' in h5:
+                print('renaming scaffolds --> chroms')
+                h5['chroms'] = h5['scaffolds']
+
+            h5.attrs['format-version'] = 1
 
