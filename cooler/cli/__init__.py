ec2454626e22244c504bae528457fb8136c59feb
Nezar <nabdennur@gmail.com>
2016-08-25 20:46:49 -0400

    Stop Click from aborting due to misconfigured locale

--- cooler/cli/__init__.py
+++ cooler/cli/__init__.py
@@ -2,9 +2,14 @@
 from __future__ import division, print_function
 import click
 
+# Monkey patch
+click.core._verify_python3_env = lambda: None
+
+
 CONTEXT_SETTINGS = {
     'help_option_names': ['-h', '--help'],
 }
+
 
 @click.group(context_settings=CONTEXT_SETTINGS)
 def cli():
