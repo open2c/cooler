7b65f5af699e969a5f962ae7728c8b0503031c39
Nezar <nabdennur@gmail.com>
2017-01-19 18:53:18 -0500

    Remove print statement

--- cooler/contrib/higlass.py
+++ cooler/contrib/higlass.py
@@ -133,7 +133,6 @@ def get_data(f, zoom_level, start_pos_1, end_pos_1, start_pos_2, end_pos_2):
     j0 = abs_coord_2_bin(c, start_pos_2, chroms, chrom_cum_lengths, chrom_sizes)
     j1 = abs_coord_2_bin(c, end_pos_2, chroms, chrom_cum_lengths, chrom_sizes)
  
-    print("zoom_level", zoom_level, i0,i1,j0,j1)
     pixels = c.matrix(as_pixels=True, max_chunk=np.inf)[i0:i1, j0:j1]
  
     if not len(pixels):
@@ -141,7 +140,7 @@ def get_data(f, zoom_level, start_pos_1, end_pos_1, start_pos_2, end_pos_2):
  
     bins = c.bins()[['chrom', 'start', 'end', 'weight']]
     pixels = annotate(pixels, bins)
-    
+
     pixels['genome_start'] = chrom_cum_lengths[pixels['chrom1']] + pixels['start1']
     pixels['genome_end'] = chrom_cum_lengths[pixels['chrom2']] + pixels['end2']
     pixels['balanced'] = (
