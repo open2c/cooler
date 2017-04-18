c5235617a6d192a9d8ed100c0e4c41e24cb67a2a
Nezar <nabdennur@gmail.com>
2017-04-18 17:29:43 -0400

    Update dask adapter

--- cooler/contrib/dask.py
+++ cooler/contrib/dask.py
@@ -1,5 +1,4 @@
 from __future__ import division, print_function
-from operator import getitem, setitem
 from math import ceil
 
 import numpy as np
@@ -13,68 +12,105 @@ import dask.dataframe as dd
 import dask.array as da
 
 
-def _coldict_accessor(clr, group_path, slc, columns, lock=None):
-    columns = list(columns)
-    if lock:
-        lock.acquire()
+def get_group_info(path, grouppath, keys):
+    with h5py.File(path, 'r') as f:
+        grp = f[grouppath]
+
+        if keys is None:
+            keys = list(grp.keys())
+        
+        nrows = len(grp[keys[0]])
+        dtypes = {key: grp[key].dtype for key in keys}
+        
+        categoricals = {}
+        for key in keys:
+            dt = h5py.check_dtype(enum=grp[key].dtype)
+            if dt is not None:
+                categoricals[key] = sorted(dt, key=dt.__getitem__)
+
+        # Meta is an empty dataframe that serves as a compound "dtype"    
+        meta = pd.DataFrame(
+            {key: np.array([], dtype=grp[key].dtype) for key in keys}, 
+            columns=keys)
+        
+    return nrows, keys, meta, categoricals
+
+
+def slice_dataset(filepath, grouppath, key, slc, lock=None):
     try:
-        with clr.open() as h5:
-            grp = h5[group_path]
-            chunks = [grp[name][slc] for name in columns]
-        data = dict(zip(columns, chunks))
+        if lock is not None:
+            lock.acquire()
+        with h5py.File(filepath, 'r') as f:
+            return f[grouppath][key][slc]
     finally:
-        if lock:
+        if lock is not None:
             lock.release()
+
+            
+def slice_group(filepath, grouppath, keys, slc, lock=None):
+    try:
+        if lock is not None:
+            lock.acquire()
+        with h5py.File(filepath, 'r') as f:
+            return {key: f[grouppath][key][slc] for key in keys}
+    finally:
+        if lock is not None:
+            lock.release()
+
+            
+def restore_categories(data, categorical_columns):
+    for key, category_dict in categorical_columns.items():
+        data[key] = pd.Categorical.from_codes(
+                data[key], 
+                categories, 
+                ordered=True)
     return data
 
 
-def _restore_categories(df, col2cat):
-    for col, category_dict in col2cat.items():
-        df[col] = pd.Categorical.from_codes(
-                df[col], sorted(category_dict, key=dt.__getitem__), ordered=True)
-    return df
+def daskify(filepath, grouppath, keys=None, chunksize=int(10e6), index=None, 
+            lock=None):
+    """
+    Create a dask dataframe around an HDF5 group containing equal-length 1D 
+    datasets serving as columns.
 
-    
-def daskify(clr, group_path, chunksize=int(10e6), columns=None, index=None, lock=None):     
-    with clr.open() as h5:
-        grp = h5[group_path]
-        if columns is None:
-            columns = tuple(grp.keys())
+    Parameters
+    ----------
+    filepath : str
+        Path to HDF5 file
+    grouppath : str
+        HDF5 group path
+    keys : list, optional
+        list of HDF5 Dataset keys, default is to use all keys in the group
+    chunksize : int, optional
+        Chunk size
+    index : str, optional
+        Sorted column to use as index
+    lock : multiprocessing.Lock, optional
+        Lock to synchronize HDF5 read/write access. Default is no lock.
 
-        shape = (len(grp[columns[0]]), len(columns))
-        dtype = np.dtype([(col, grp[col].dtype) for col in columns])
-        ncols = len(columns)
-        nrows = shape[0]
+    """
+    nrows, keys, meta, categoricals = get_group_info(filepath, grouppath, keys)
     
-        # Meta is an empty dataframe that serves as a compound "dtype"    
-        meta = pd.DataFrame(
-            {col: np.array([], dtype=grp[col].dtype) for col in columns}, 
-            columns=columns)
-    
-        col2cat = {}
-        for col in columns:
-            dt = h5py.check_dtype(enum=grp[col].dtype)
-            if dt is not None:
-                meta[col] = pd.Categorical.from_codes(
-                    meta[col], sorted(dt, key=dt.__getitem__), ordered=True)
-                col2cat[col] = dt
+    # Make a unique task name
+    token = tokenize(filepath, grouppath, chunksize, keys)
+    task_name = 'daskify-h5py-table' + token
 
     # Partition the table
     divisions = (0,) + tuple(range(-1, nrows, chunksize))[1:]             
     if divisions[-1] != nrows - 1:
-        divisions = divisions + (nrows - 1,)
+        divisions = divisions + (nrows - 1,) 
     
-    # Make a unique task name
-    token = tokenize(id(clr), group_path, chunksize, columns)
-    task_name = 'cooler-daskify-' + token
-
-    # Create the task graph
+    # Build the task graph
     dsk = {}
     for i in range(0, int(ceil(nrows / chunksize))):
         slc = slice(i * chunksize, (i + 1) * chunksize)
-        data = (_coldict_accessor, clr, group_path, slc, columns, lock)
-        if col2cat:
-            data = (_restore_categories, data, col2cat)
-        dsk[task_name, i] = (pd.DataFrame, data, None, meta.columns)
-    
-    return new_dd_object(dsk, task_name, meta, divisions)
+        data_dict = (slice_group, filepath, grouppath, keys, slc, lock)
+        if categoricals:
+            data_dict = (restore_categories, data_dict, categoricals)
+        dsk[task_name, i] = (pd.DataFrame, data_dict, None, meta.columns)
+
+    # Generate ddf from dask graph
+    df = new_dd_object(dsk, task_name, meta, divisions)
+    if index is not None:
+        df = df.set_index(index, sorted=True, drop=False)
+    return df
