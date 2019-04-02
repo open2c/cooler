b021ed153ddb07e0408606b98705ea332d73eb9d
Nezar <nabdennur@gmail.com>
2019-04-02 18:13:08 -0400

    Add tests for coarsen's partitioning

--- tests/test_reduce.py
+++ tests/test_reduce.py
@@ -13,11 +13,12 @@ import pytest
 
 
 testdir = op.realpath(op.dirname(__file__))
+datadir = op.join(testdir, 'data')
 
 
 @pytest.mark.parametrize("path1,path2", [(
-    op.join(testdir, 'data', 'hg19.GM12878-MboI.matrix.2000kb.cool'),
-    op.join(testdir, 'data', 'hg19.GM12878-MboI.matrix.2000kb.cool')
+    op.join(datadir, 'hg19.GM12878-MboI.matrix.2000kb.cool'),
+    op.join(datadir, 'hg19.GM12878-MboI.matrix.2000kb.cool')
 )])
 def test_merge(path1, path2):
     with isolated_filesystem():
@@ -28,7 +29,7 @@ def test_merge(path1, path2):
 
 
 def test_recursive_agg():
-    infile = op.join(testdir, 'data', 'hg19.GM12878-MboI.matrix.2000kb.cool')
+    infile = op.join(datadir, 'hg19.GM12878-MboI.matrix.2000kb.cool')
     chunksize = int(10e6)
     n_zooms = 2
     n_cpus = 1
@@ -37,12 +38,12 @@ def test_recursive_agg():
 
 
 @pytest.mark.parametrize("input_uri,factor,ref_uri", [
-    (op.join(testdir, 'data', 'toy.symm.upper.2.cool'), 2,
-     op.join(testdir, 'data', 'toy.symm.upper.4.cool')),
-    (op.join(testdir, 'data', 'toy.asymm.2.cool'), 2,
-     op.join(testdir, 'data', 'toy.asymm.4.cool')),
-    (op.join(testdir, 'data', 'toy.symm.upper.var.cool'), 2,
-     op.join(testdir, 'data', 'toy.symm.upper.var2x.cool'))
+    (op.join(datadir, 'toy.symm.upper.2.cool'), 2,
+     op.join(datadir, 'toy.symm.upper.4.cool')),
+    (op.join(datadir, 'toy.asymm.2.cool'), 2,
+     op.join(datadir, 'toy.asymm.4.cool')),
+    (op.join(datadir, 'toy.symm.upper.var.cool'), 2,
+     op.join(datadir, 'toy.symm.upper.var2x.cool'))
     ])
 def test_coarsen(input_uri, factor, ref_uri):
     kwargs = dict(
@@ -62,6 +63,30 @@ def test_coarsen(input_uri, factor, ref_uri):
         cooler_cmp('test.cool', ref_uri)
 
 
+def test_coarsen_partitions_correctly():
+    kwargs = dict(
+        nproc=1,
+        columns=None,
+        dtypes=None,
+        agg=None
+    )
+    with isolated_filesystem():
+        f_ref = op.join(datadir, 'odd.4.cool')
+        f_in = op.join(datadir, 'odd.1.cool')
+        coarsen_cooler(
+            f_in,
+            'odd.1.coarsen_4.cool',
+            factor=4,
+            chunksize=2,
+            **kwargs
+        )
+        pix1 = cooler.Cooler(f_ref).pixels()[:]
+        pix2 = cooler.Cooler('odd.1.coarsen_4.cool').pixels()[:]
+        assert len(pix1) == len(pix2)
+        assert sum(pix2[['bin1_id', 'bin2_id']].duplicated()) == 0
+        assert np.allclose(pix1, pix2)
+
+
 def test_zoomify():
     kwargs = dict(
         chunksize=10,
@@ -72,7 +97,7 @@ def test_zoomify():
     )
     with isolated_filesystem():
         zoomify_cooler(
-            op.join(testdir, 'data', 'toy.asymm.2.cool'),
+            op.join(datadir, 'toy.asymm.2.cool'),
             'test.2.mcool',
             resolutions=[4, 8, 16, 32],
             **kwargs
@@ -80,12 +105,12 @@ def test_zoomify():
         for res in [2, 4, 8, 16, 32]:
             cooler_cmp(
                 'test.2.mcool::resolutions/{}'.format(res),
-                op.join(testdir, 'data', 'toy.asymm.{}.cool'.format(res)))
+                op.join(datadir, 'toy.asymm.{}.cool'.format(res)))
 
 
 # def test_zoomify():
 #     zoomify(
-#         op.join(testdir, 'data',
+#         op.join(datadir,
 #             'dec2_20_pluslig_1pGene_grch38_UBR4_D_1nt.pairwise.sorted.cool'),
 #         out=multires_path,
 #         balance=True,
