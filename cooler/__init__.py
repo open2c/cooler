fa1bac25e96d693ac40e1cbe2b2be0cc511173ef
Nezar Abdennur <nabdennur@gmail.com>
2021-02-03 09:36:45 -0500

    Add system of logging contexts and verbosity

--- cooler/__init__.py
+++ cooler/__init__.py
@@ -10,6 +10,7 @@ A cool place to store your Hi-C.
 
 """
 from ._version import __version__, __format_version__
+from ._logging import get_verbosity_level, set_verbosity_level
 from .api import Cooler, annotate
 from .create import create_cooler, rename_chroms, create_scool
 from .reduce import merge_coolers, coarsen_cooler, zoomify_cooler
--- cooler/cli/__init__.py
+++ cooler/cli/__init__.py
@@ -1,8 +1,9 @@
 import logging
 import sys
 from .._version import __version__
-from .._logging import get_logger
+from .._logging import get_logger, set_logging_context, set_verbosity_level
 import click
+
 
 # Monkey patch
 click.core._verify_python3_env = lambda: None
@@ -31,63 +32,56 @@ def cli(verbose, debug):
     Type -h or --help after any subcommand for more information.
 
     """
-    # Initialize logging to stderr
-    logging.basicConfig(stream=sys.stderr)
-    logging.captureWarnings(True)
-    root_logger = get_logger()
+    set_logging_context("cli")
+    set_verbosity_level(verbose + 1)
+    logger = get_logger()
 
-    # Set verbosity level
-    if verbose > 0:
-        root_logger.setLevel(logging.DEBUG)
-        if verbose > 1:  # pragma: no cover
-            try:
-                import psutil
-                import atexit
+    if verbose > 1:  # pragma: no cover
+        try:
+            import psutil
+            import atexit
 
-                @atexit.register
-                def process_dump_at_exit():
-                    process_attrs = [
-                        "cmdline",
-                        # 'connections',
-                        "cpu_affinity",
-                        "cpu_num",
-                        "cpu_percent",
-                        "cpu_times",
-                        "create_time",
-                        "cwd",
-                        # 'environ',
-                        "exe",
-                        # 'gids',
-                        "io_counters",
-                        "ionice",
-                        "memory_full_info",
-                        # 'memory_info',
-                        # 'memory_maps',
-                        "memory_percent",
-                        "name",
-                        "nice",
-                        "num_ctx_switches",
-                        "num_fds",
-                        "num_threads",
-                        "open_files",
-                        "pid",
-                        "ppid",
-                        "status",
-                        "terminal",
-                        "threads",
-                        # 'uids',
-                        "username",
-                    ]
-                    p = psutil.Process()
-                    info_ = p.as_dict(process_attrs, ad_value="")
-                    for key in process_attrs:
-                        root_logger.debug("PSINFO:'{}': {}".format(key, info_[key]))
+            @atexit.register
+            def process_dump_at_exit():
+                process_attrs = [
+                    "cmdline",
+                    # 'connections',
+                    "cpu_affinity",
+                    "cpu_num",
+                    "cpu_percent",
+                    "cpu_times",
+                    "create_time",
+                    "cwd",
+                    # 'environ',
+                    "exe",
+                    # 'gids',
+                    "io_counters",
+                    "ionice",
+                    "memory_full_info",
+                    # 'memory_info',
+                    # 'memory_maps',
+                    "memory_percent",
+                    "name",
+                    "nice",
+                    "num_ctx_switches",
+                    "num_fds",
+                    "num_threads",
+                    "open_files",
+                    "pid",
+                    "ppid",
+                    "status",
+                    "terminal",
+                    "threads",
+                    # 'uids',
+                    "username",
+                ]
+                p = psutil.Process()
+                info_ = p.as_dict(process_attrs, ad_value="")
+                for key in process_attrs:
+                    logger.debug("PSINFO:'{}': {}".format(key, info_[key]))
 
-            except ImportError:
-                root_logger.warning("Install psutil to see process information.")
-
-    else:
-        root_logger.setLevel(logging.INFO)
+        except ImportError:
+            logger.warning("Install psutil to see process information.")
 
     # Set hook for postmortem debugging
     if debug:  # pragma: no cover
