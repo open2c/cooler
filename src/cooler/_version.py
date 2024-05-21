49ecab7a5395c43719e26dba3d79371c3ed2b494
Nezar Abdennur <nabdennur@gmail.com>
2024-05-20 21:31:37 -0400

    maint: Use importlib and static version in pyproject

--- src/cooler/_version.py
+++ src/cooler/_version.py
@@ -1,4 +1,13 @@
-__version__ = "0.9.3"
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("cooler")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 __format_version__ = 3
 __format_version_mcool__ = 2
 __format_version_scool__ = 1
